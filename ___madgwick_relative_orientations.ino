0997cb0981fde77b22fca15b2b7b15e976ffb329
ShadyRaccoon <marianbogdan953@yahoo.com>
2025-06-07 02:41:21 +0300

    clean data gathering form imu's

--- ___madgwick_relative_orientations.ino
+++ ___madgwick_relative_orientations.ino
@@ -1,155 +1,89 @@
 #include <Wire.h>
+#include <Preferences.h>
 #include <MadgwickAHRS.h>
 
-#define RED_PIN 5
-#define GREEN_PIN 23
-#define BLUE_PIN 18 
+#define CAL_SAMPLES 150
 
-#define MUX_ADDRESS 0x70
+#define FA 0
+#define UA 2
+#define PALM 7
 
-#define PALM_CHANNEL 7
-#define UA_CHANNEL 2
-#define FA_CHANNEL 0
+#define INTERVAL 10
+#define SAMPLE_FREQ 100
 
-#define GYRO_CALIBRATION_SAMPLES 150
+Preferences prefs;
 
-//data  -> rotation rate & acceleration on the axis
-float aX_palm, aY_palm, aZ_palm, gX_palm, gY_palm, gZ_palm; //palm  data
-float aX_fa, aY_fa, aZ_fa, gX_fa, gY_fa, gZ_fa;          //forearm  data
-float aX_ua, aY_ua, aZ_ua, gX_ua, gY_ua, gZ_ua;             //upper arm  data
+long last_print = 0;
 
-//orientation quaternion
-float q0_palm, q1_palm, q2_palm, q3_palm; //palm quaternion
-float q0_fa, q1_fa, q2_fa, q3_fa;         //forearm quaternion
-float q0_ua, q1_ua, q2_ua, q3_ua;         //upper arm quaternion
+float gX_palm, gY_palm, gZ_palm;
+float gX_fa, gY_fa, gZ_fa;
+float gX_ua, gY_ua, gZ_ua;
 
-//avg axis measurement error / offsets for gyro 
 float offX_palm, offY_palm, offZ_palm;
 float offX_fa, offY_fa, offZ_fa;
 float offX_ua, offY_ua, offZ_ua;
 
-//filter handler
+float aX_palm, aY_palm, aZ_palm;
+float aX_fa, aY_fa, aZ_fa;
+float aX_ua, aY_ua, aZ_ua;
+
+float q0_palm, q1_palm, q2_palm, q3_palm;
+float q0_fa, q1_fa, q2_fa, q3_fa;
+float q0_ua, q1_ua, q2_ua, q3_ua;
+
+float roll_palm, pitch_palm, yaw_palm;
+float roll_fa, pitch_fa, yaw_fa;
+float roll_ua, pitch_ua, yaw_ua;
+
 Madgwick filter_palm, filter_fa, filter_ua;
 
-//function to select multiplexer
-void mux_select(uint8_t i2c_bus){
-  if (i2c_bus > 7) return;
-  Wire.beginTransmission(MUX_ADDRESS);
-  Wire.write(1 << i2c_bus);
-  Wire.endTransmission();
-}
-
 void mpu_config(){
-  Wire.beginTransmission(0x68); // Adresa I2C a MPU6050
-  Wire.write(0x1A); // Setare LPF pentru accelerometru
-  Wire.write(0x05);
-  Wire.endTransmission();
-
-  Wire.beginTransmission(0x68); // Setare scală de măsurare pentru accelerometru
-  Wire.write(0x1C);
-  Wire.write(0x10); // [-8g ; +8g]
-  Wire.endTransmission();
-
-  // Configurare MPU6050 pentru giroscop
-  Wire.beginTransmission(0x68); // Adresa I2C a MPU6050
-  Wire.write(0x1A); // Setare LPF pentru giroscop
-  Wire.write(0x05);
-  Wire.endTransmission();
-
-  Wire.beginTransmission(0x68); // Setare scală de măsurare pentru giroscop
-  Wire.write(0x1B);
-  Wire.write(0x08); // [-500deg/s ; +500deg/s]
-  Wire.endTransmission();
-
-  // Ieșire din modul de sleep pentru MPU6050
   Wire.beginTransmission(0x68);
-  Wire.write(0x6B); // Adresa registrului de control al puterii
-  Wire.write(0x00); // Dezactivare mod sleep
+  Wire.write(0x6B);
+  Wire.write(0x00); //sleep bit -> 0
+  Wire.endTransmission();
+
+  Wire.beginTransmission(0x68); //config gyro range
+  Wire.write(0x1B);
+  Wire.write(0x8);
+  Wire.endTransmission();
+
+  Wire.beginTransmission(0x68); //turn on LPF
+  Wire.write(0x1A);
+  Wire.write(0x05);
+  Wire.endTransmission();
+
+  Wire.beginTransmission(0x68); //config accel range
+  Wire.write(0x1C);
+  Wire.write(0x10);
   Wire.endTransmission();
 }
 
-void get_gyro_offsets(uint8_t channel, float &offX, float &offY, float &offZ){
-  float x = 0, y = 0, z = 0;
-  float aX, aY, aZ, gX, gY, gZ;
-
-  for(int i = 0 ; i < GYRO_CALIBRATION_SAMPLES ; i++){
-    get_raw_data(channel, aX, aY, aZ, gX, gY, gZ);
-    x += gX;
-    y += gY;
-    z += gZ;
-  }
-
-  offX = x / GYRO_CALIBRATION_SAMPLES;
-  offY = y / GYRO_CALIBRATION_SAMPLES;
-  offZ = z / GYRO_CALIBRATION_SAMPLES;
+void mux_channel(uint8_t channel){
+  Wire.beginTransmission(0x70);
+  Wire.write(1 << channel);
+  Wire.endTransmission();
 }
 
-void calibrate(float &cal_gX, float &cal_gY, float &cal_gZ, float offX, float offY, float offZ){
-  cal_gX -= offX;
-  cal_gY -= offY;
-  cal_gZ -= offZ;
+void gyro_data(float *gX, float *gY, float *gZ){
+  Wire.beginTransmission(0x68);
+  Wire.write(0x43);
+  Wire.endTransmission();
+
+  Wire.requestFrom(0x68,6);
+
+  int16_t rawX = Wire.read() << 8 | Wire.read();
+  int16_t rawY = Wire.read() << 8 | Wire.read();
+  int16_t rawZ = Wire.read() << 8 | Wire.read();
+
+  //convert raw value to deg/s
+  *gX = (float)rawX / 65.5;
+  *gY = (float)rawY / 65.5;
+  *gZ = (float)rawZ / 65.5;
 }
 
-void get_raw_data(uint8_t channel, float &aX, float &aY, float &aZ, float &gX, float &gY, float &gZ){
-  //citire date accel
-  mux_select(channel);
-  Wire.beginTransmission(0x68); // Adresa I2C a MPU6050
-  Wire.write(0x3B); // Adresa pentru datele de accelerometru
-  // 3B - 40 -> accel
-  // 41 - 42 -> temp
-  // 43 - 48 -> gyro
-  if(Wire.endTransmission() != 0){
-    Serial.println("Comunicare I2C esuata.");
-    return;
-  };
-
-  Wire.requestFrom(0x68, 14); // Cerere de date de la MPU6050
-
-  if(Wire.available() < 14){
-    Serial.println("Date indisponibile.");
-    return;
-  }
-  
-  // Citire date accelerometru
-  int16_t raw_aX = Wire.read() << 8 | Wire.read();
-  int16_t raw_aY = Wire.read() << 8 | Wire.read();
-  int16_t raw_aZ = Wire.read() << 8 | Wire.read();
-
-  aX = float(raw_aX) / 4096;
-  aY = float(raw_aY) / 4096;
-  aZ = float(raw_aZ) / 4096;
-
-  //skip registri temperatura
-  Wire.read();
-  Wire.read();
-
-  // Citire date giroscop
-  int16_t raw_gX = Wire.read() << 8 | Wire.read();
-  int16_t raw_gY = Wire.read() << 8 | Wire.read();
-  int16_t raw_gZ = Wire.read() << 8 | Wire.read();
-
-  gX = float(raw_gX) / 65.5;
-  gY = float(raw_gY) / 65.5;
-  gZ = float(raw_gZ) / 65.5;
-}
-
-void print_data(char* segment, float aX, float aY, float aZ, float gX, float gY, float gZ){
-  Serial.print("Acceleratia pe X pentru ");
-  Serial.print(segment);
-  Serial.print(" este: ");
-  Serial.println(aX);
-
-  Serial.print("Acceleratia pe Y pentru ");
-  Serial.print(segment);
-  Serial.print(" este: ");
-  Serial.println(aY);
-
-  Serial.print("Acceleratia pe Z pentru ");
-  Serial.print(segment);
-  Serial.print(" este: ");
-  Serial.println(aZ);
-
-  Serial.print("Viteza ungiulara pe X pentru ");
+void print_gyro(char * segment, float gX, float gY, float gZ){
+  Serial.print("Viteza unghiulara pe X pentru ");
   Serial.print(segment);
   Serial.print(" este: ");
   Serial.println(gX);
@@ -165,44 +99,223 @@ void print_data(char* segment, float aX, float aY, float aZ, float gX, float gY,
   Serial.println(gZ);
 }
 
+void get_gyro_offsets(float *offX, float *offY, float *offZ){
+  float x = 0, y = 0, z = 0;
+  float gX, gY, gZ;
+  for(int i = 0 ; i < CAL_SAMPLES ; i++){
+    gyro_data(&gX, &gY, &gZ);
+    x += gX;
+    y += gY;
+    z += gZ;
+  }
+
+  *offX = (float) x / CAL_SAMPLES;
+  *offY = (float) y / CAL_SAMPLES;
+  *offZ = (float) z / CAL_SAMPLES; 
+}
+
+void calibrate_gyro(float *gX, float *gY, float *gZ, float offX, float offY, float offZ){
+  *gX -= offX;
+  *gY -= offY;
+  *gZ -= offZ;
+}
+
+void accel_data(float *aX, float *aY, float *aZ){
+  Wire.beginTransmission(0x68);
+  Wire.write(0x3B);
+  Wire.endTransmission();
+
+  Wire.requestFrom(0x68,6);
+
+  int16_t rawX = Wire.read() << 8 | Wire.read();
+  int16_t rawY = Wire.read() << 8 | Wire.read();
+  int16_t rawZ = Wire.read() << 8 | Wire.read();
+
+  //convert raw value to deg/s
+  *aX = (float)rawX / 4096;
+  *aY = (float)rawY / 4096;
+  *aZ = (float)rawZ / 4096;
+}
+
+void print_accel(char *segment, float aX, float aY, float aZ){
+  Serial.print("Acceleratia pe X pentru ");
+  Serial.print(segment);
+  Serial.print(" este: ");
+  Serial.println(aX);
+
+  Serial.print("Acceleratia pe Y pentru ");
+  Serial.print(segment);
+  Serial.print(" este: ");
+  Serial.println(aY);
+
+  Serial.print("Acceleratia pe Z pentru ");
+  Serial.print(segment);
+  Serial.print(" este: ");
+  Serial.println(aZ);
+}
+
+void print_angles_quaternions(char *segment, float q0, float q1, float q2, float q3, float roll, float pitch, float yaw){
+  Serial.printf(" Quaternion (%s): [%.3f  %.3f  %.3f  %.3f]\n", segment, q0, q1, q2, q3);
+
+  Serial.printf(" Euler R/P/Y (%s): [%.1f°,  %.1f°,  %.1f°]\n\n", segment, roll, pitch, yaw);
+}
+
 void setup() {
   // put your setup code here, to run once:
+
   Serial.begin(115200);
 
-  Wire.begin();
   Wire.setClock(400000);
+  Wire.begin();
+  delay(250);
 
-  mux_select(PALM_CHANNEL);
-  mpu_config();
-  get_gyro_offsets(PALM_CHANNEL, offX_palm, offY_palm, offZ_palm);
+  prefs.begin("gyroCal", false);
+  bool isCalibrated = prefs.getBool("calibrated", false);
 
-  mux_select(FA_CHANNEL);
-  mpu_config();
-  get_gyro_offsets(FA_CHANNEL, offX_fa, offY_fa, offZ_fa);
+  if(!isCalibrated){
+    Serial.println("Senzori necalibrati...");
+    Serial.println("Calibrare...");
 
-  mux_select(UA_CHANNEL);
-  mpu_config();
-  get_gyro_offsets(UA_CHANNEL, offX_ua, offY_ua, offZ_ua);
+    Serial.println("Lasati senzorii nemiscati timp de 5s");
+    delay(5000);
 
-  delay(300);
+    mux_channel(PALM);
+    delay(10);
+    mpu_config();
+    delay(10);
+    get_gyro_offsets(&offX_palm, &offY_palm, &offZ_palm);
+
+    Serial.println("== Calibrare senzor antebrat ==");
+    mux_channel(FA);
+    delay(10);
+    mpu_config();
+    delay(10);
+    get_gyro_offsets(&offX_fa, &offY_fa, &offZ_fa);
+
+    Serial.println("== Calibrare senzor brat ==");
+    mux_channel(UA);
+    delay(10);
+    mpu_config();
+    delay(10);
+    get_gyro_offsets(&offX_ua, &offY_ua, &offZ_ua);
+
+    Serial.println("== Calibrare completa. Salvare in flash... ==");
+
+    prefs.putFloat("pX", offX_palm);
+    prefs.putFloat("pY", offY_palm);
+    prefs.putFloat("pZ", offZ_palm);
+
+    prefs.putFloat("fX", offX_fa);
+    prefs.putFloat("fY", offY_fa);
+    prefs.putFloat("fZ", offZ_fa);
+
+    prefs.putFloat("uX", offX_ua);
+    prefs.putFloat("uY", offY_ua);
+    prefs.putFloat("uZ", offZ_ua);
+
+    prefs.putBool("calibrated", true);
+    prefs.end();
+
+    Serial.println(">>> Date de calibrare salvate. Reporniti alimentarea <<<");
+    while(true){}
+  }else{
+    mux_channel(PALM);
+    delay(10);
+    mpu_config();
+    delay(10);
+
+    mux_channel(FA);
+    delay(10);
+    mpu_config();
+    delay(10);
+
+    mux_channel(UA);
+    delay(10);
+    mpu_config();
+    delay(10);
+
+    Serial.println("== Calibrare... ==");
+
+    offX_palm = prefs.getFloat("pX", 0.0f);
+    offY_palm = prefs.getFloat("pY", 0.0f);
+    offZ_palm = prefs.getFloat("pZ", 0.0f);
+
+    offX_fa   = prefs.getFloat("fX", 0.0f);
+    offY_fa   = prefs.getFloat("fY", 0.0f);
+    offZ_fa   = prefs.getFloat("fZ", 0.0f);
+
+    offX_ua   = prefs.getFloat("uX", 0.0f);
+    offY_ua   = prefs.getFloat("uY", 0.0f);
+    offZ_ua   = prefs.getFloat("uZ", 0.0f);
+
+    prefs.end();
+
+    Serial.println(">>> Senzori calibrati <<<");
+
+    filter_palm.begin(SAMPLE_FREQ);
+    filter_fa.begin(SAMPLE_FREQ);
+    filter_ua.begin(SAMPLE_FREQ);
+  }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  get_raw_data(PALM_CHANNEL, aX_palm, aY_palm, aZ_palm, gX_palm, gY_palm, gZ_palm);
-  calibrate(gX_palm, gY_palm, gZ_palm, offX_palm, offY_palm, offZ_palm);
+  mux_channel(PALM);
+  delay(5);
+  gyro_data(&gX_palm, &gY_palm, &gZ_palm);
+  accel_data(&aX_palm, &aY_palm, &aZ_palm);
+  calibrate_gyro(&gX_palm, &gY_palm, &gZ_palm, offX_palm, offY_palm, offZ_palm);
   filter_palm.updateIMU(gX_palm, gY_palm, gZ_palm, aX_palm, aY_palm, aZ_palm);
-  print_data("palma", aX_palm, aY_palm, aZ_palm, gX_palm, gY_palm, gZ_palm);
+  q0_palm = filter_palm.getQuaternion0();
+  q1_palm = filter_palm.getQuaternion1();
+  q2_palm = filter_palm.getQuaternion2();
+  q3_palm = filter_palm.getQuaternion3();
+  roll_palm  = filter_palm.getRoll();
+  pitch_palm = filter_palm.getPitch();
+  yaw_palm   = filter_palm.getYaw();
 
-  get_raw_data(FA_CHANNEL, aX_fa, aY_fa, aZ_fa, gX_fa, gY_fa, gZ_fa);
-  calibrate(gX_fa, gY_fa, gZ_fa, offX_fa, offY_fa, offZ_fa);
+  mux_channel(FA);
+  delay(5);
+  gyro_data(&gX_fa, &gY_fa, &gZ_fa);
+  accel_data(&aX_fa, &aY_fa, &aZ_fa);
+  calibrate_gyro(&gX_fa, &gY_fa, &gZ_fa, offX_fa, offY_fa, offZ_fa);
   filter_fa.updateIMU(gX_fa, gY_fa, gZ_fa, aX_fa, aY_fa, aZ_fa);
-  print_data("antebrat", aX_fa, aY_fa, aZ_fa, gX_fa, gY_fa, gZ_fa);
+  q0_fa = filter_fa.getQuaternion0();
+  q1_fa = filter_fa.getQuaternion1();
+  q2_fa = filter_fa.getQuaternion2();
+  q3_fa = filter_fa.getQuaternion3();
+  roll_fa  = filter_fa.getRoll();
+  pitch_fa = filter_fa.getPitch();
+  yaw_fa   = filter_fa.getYaw();
 
-  get_raw_data(UA_CHANNEL, aX_ua, aY_ua, aZ_ua, gX_ua, gY_ua, gZ_ua);
-  calibrate(gX_ua, gY_ua, gZ_ua, offX_ua, offY_ua, offZ_ua);
-  filter_fa.updateIMU(gX_ua, gY_ua, gZ_ua, aX_ua, aY_ua, aZ_ua);
-  print_data("bratul superior", aX_ua, aY_ua, aZ_ua, gX_ua, gY_ua, gZ_ua);
+  mux_channel(UA);
+  delay(5);
+  gyro_data(&gX_ua, &gY_ua, &gZ_ua);
+  accel_data(&aX_ua, &aY_ua, &aZ_ua);
+  calibrate_gyro(&gX_ua, &gY_ua, &gZ_ua, offX_ua, offY_ua, offZ_ua);
+  filter_ua.updateIMU(gX_ua, gY_ua, gZ_ua, aX_ua, aY_ua, aZ_ua);
+  q0_ua = filter_ua.getQuaternion0();
+  q1_ua = filter_ua.getQuaternion1();
+  q2_ua = filter_ua.getQuaternion2();
+  q3_ua = filter_ua.getQuaternion3();
+  roll_ua  = filter_ua.getRoll();
+  pitch_ua = filter_ua.getPitch();
+  yaw_ua   = filter_ua.getYaw();
 
-  delay(500);
+  if( millis() - last_print >= 1000){
+    print_gyro("palma", gX_palm, gY_palm, gZ_palm);
+    print_accel("palma", aX_palm, aY_palm, aZ_palm);
+    print_angles_quaternions("palma ", q0_palm, q1_palm, q2_palm, q3_palm, rofll_palm, pitch_palm, yaw_palm);
+    
+    print_gyro("antebrat", gX_fa, gY_fa, gZ_fa);
+    print_accel("antebrat", aX_fa, aY_fa, aZ_fa);
+    print_angles_quaternions("antebrat ", q0_fa, q1_fa, q2_fa, q3_fa, roll_fa, pitch_fa, yaw_fa);
+
+    print_gyro("brat", gX_ua, gY_ua, gZ_ua);
+    print_accel("brat", aX_ua, aY_ua, aZ_ua);
+    print_angles_quaternions("brat ", q0_ua, q1_ua, q2_ua, q3_ua, roll_ua, pitch_ua, yaw_ua);
+
+    last_print = millis();  
+  }
+  //delay(500);
 }
